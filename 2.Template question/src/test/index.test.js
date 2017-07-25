cb04159e81b9fbcddda6fea8e276204690121211
Idan.Ofek <edanofek@gmail.com>
2017-07-26 00:12:51 +0300

    Create test for mock template creation

---
+++ 2.Template question/src/test/index.test.js
@@ -0,0 +1,12 @@
+describe({
+    it('should create mock template',{
+        //1.create mock template 
+        //2.check for equal mock template
+    });
+
+    it('there should be 3 li elemnets  ',{
+        //1.create mock template 
+        //2.check for equal mock template with 3 li elems
+    });
+    
+});
