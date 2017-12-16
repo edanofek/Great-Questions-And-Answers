62714306ab0f96db726a032ea0c266a2ca2b7a59
IOGHub <edanofek@gmail.com>
2017-12-16 23:49:16 +0200

    Enter base build for MisspelledCharacter question

---
+++ 3.Logic question/src/Main.java
@@ -0,0 +1,15 @@
+// package src;
+
+
+public class Main {
+
+	// private static tree () {
+		
+	// }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("idan");
+	}
+
+}
---
+++ 3.Logic question/test/Main.Test.java
@@ -0,0 +1,16 @@
+// package src;
+
+
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MainTests {
+    private 
+}
