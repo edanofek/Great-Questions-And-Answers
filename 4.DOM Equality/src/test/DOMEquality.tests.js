5c0a06e4745233316d493ecb1ab51d14d0bbabe5
IOGHub <edanofek@gmail.com>
2018-01-02 01:30:25 +0200

    Updae node in are equal test

--- 4.DOM Equality/src/test/DOMEquality.tests.js
+++ 4.DOM Equality/src/test/DOMEquality.tests.js
@@ -36,16 +36,16 @@ QUnit.test("test DOMEquality areEqual(element1, element2) method", function( ass
     var areEqual = new DOMEquality.DOMEquality().areEqual;
     
     var mockElemDiv1 = document.createElement("div");
-    mockElemDiv1.appendChild("img");  
-    mockElemDiv1.appendChild("span");  
-    mockElemDiv1.appendChild("div").appendChild("img");
+    mockElemDiv1.appendChild(document.createElement("img"));  
+    mockElemDiv1.appendChild(document.createElement("span"));  
+    mockElemDiv1.appendChild(document.createElement("div")).appendChild(document.createElement("img"));
 
     var mockElemDiv2 = document.createElement("div");
-    mockElemDiv2.appendChild("img");
-    mockElemDiv2.appendChild("img");
-    mockElemDiv2.appendChild("span").appendChild("div");
+    mockElemDiv2.appendChild(document.createElement("img"));
+    mockElemDiv2.appendChild(document.createElement("img"));
+    mockElemDiv2.appendChild(document.createElement("span")).appendChild(document.createElement("div"));
 
-    var mockElemDiv3 = document.createElement("div").appendChild("span");
+    var mockElemDiv3 = document.createElement("div").appendChild(document.createElement("span"));
 
     assert.equal(areEqual(mockElemDiv1,mockElemDiv2),true ,"are equal method is true for elem 1,elem 2");
     assert.ok(areEqual(mockElem1,mockElemDiv3), false ,"are equal method is false for elem 1,elem 3");
