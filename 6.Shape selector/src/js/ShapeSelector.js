37e0490f6f6c516db9ddc94aa453cdb6bd96f35f
IOGHub <edanofek@gmail.com>
2018-01-11 22:21:39 +0200

    Add shape selector DS

---
+++ 6.Shape selector/src/js/ShapeSelector.js
@@ -0,0 +1,20 @@
+var ShapeSelector = (function(){
+
+    //hold the shapes ds
+    var ShapesDS ={};
+
+    var ShapeSelector = function(){
+        
+    }
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image
+    ShapeSelector.prototype.initFN = function(fullOrEmpty){
+        if(fullOrEmpty === true){
+
+        }
+    }
+
+    return {
+        ShapeSelector:ShapeSelector,
+        ShapesDS:ShapesDS
+    };
+})();
