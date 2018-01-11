d5f9e55540fa8dea9754343b19a29dade63cdc83
IOGHub <edanofek@gmail.com>
2018-01-12 00:27:29 +0200

    Add image holder class

--- 6.Shape selector/src/js/ShapeSelector.js
+++ 6.Shape selector/src/js/ShapeSelector.js
@@ -1,20 +1,68 @@
-var ShapeSelector = (function(){
+// Hold Our Images 
+var ImageHolderContianer = (function(){
+    
 
-    //hold the shapes ds
-    var ShapesDS ={};
+    var ImageHolderContianer = function(shapeSelectorID){
+        
+        this.shapeSelectorID = shapeSelectorID
+        this.imageHolderDS = {};
+    };
 
-    var ShapeSelector = function(){
+
+    ImageHolderContianer.prototype.addImages = function(){
+        this.addImage = function(imageName,shapeSelectorID){
+
+            var loadedImage = new Image();
+            loadedImage.src = "./src/img/"+imageName+".png";
+            this.imageHolderDS[imageName]=
+                    {selected:false,
+                    loadedImageElem:loadedImage,
+                    shapeSelectorID:shapeSelectorID};
+            
+        }
+
+        this.addImage('ellipsis',this.shapeSelectorID);
+        this.addImage('polygon',this.shapeSelectorID);
+        this.addImage('reactangle',this.shapeSelectorID);
+        this.addImage('rounded_rect',this.shapeSelectorID);
+        this.addImage('star_big',this.shapeSelectorID);
+        this.addImage('star_small',this.shapeSelectorID);
+        this.addImage('triangle',this.shapeSelectorID);
         
     }
-    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/Image
+
+    return {
+        ImageHolderContianer:ImageHolderContianer,
+    }
+})();
+
+
+
+var ShapeSelector = (function(){
+
+    //hold the shapes ds on each shape selector
+    
+
+    var ShapeSelector = function(shapeSelectorID){
+        // Cto'r
+        var shapeSelectorID = this.shapeSelectorID;
+    }
+
     ShapeSelector.prototype.initFN = function(fullOrEmpty){
+     
+        // var DrawBase
+
         if(fullOrEmpty === true){
+
+
+        }else{
 
         }
     }
 
     return {
         ShapeSelector:ShapeSelector,
-        ShapesDS:ShapesDS
+        // ShapesDS:ShapesDS
     };
+
 })();
