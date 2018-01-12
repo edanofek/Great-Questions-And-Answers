dc6d5ac43856ceeeb69350752d960c73459a5e5f
IOGHub <edanofek@gmail.com>
2018-01-12 02:08:57 +0200

    Update Shape Selector JS/Style

--- 6.Shape selector/src/js/ShapeSelector.js
+++ 6.Shape selector/src/js/ShapeSelector.js
@@ -1,7 +1,8 @@
 // Hold Our Images 
+// Image Holder container - class
 var ImageHolderContianer = (function(){
     
-
+    // Cto'r 
     var ImageHolderContianer = function(shapeSelectorID){
         
         this.shapeSelectorID = shapeSelectorID
@@ -10,18 +11,18 @@ var ImageHolderContianer = (function(){
 
 
     ImageHolderContianer.prototype.addImages = function(){
-        this.addImage = function(imageName,shapeSelectorID){
+        this.addImage = function(imageName,shapeSelectorID,selected){
 
             var loadedImage = new Image();
             loadedImage.src = "./src/img/"+imageName+".png";
             this.imageHolderDS[imageName]=
-                    {selected:false,
+                    {selected:selected ? true : false,
                     loadedImageElem:loadedImage,
                     shapeSelectorID:shapeSelectorID};
             
         }
 
-        this.addImage('ellipsis',this.shapeSelectorID);
+        this.addImage('ellipsis',this.shapeSelectorID,true);
         this.addImage('polygon',this.shapeSelectorID);
         this.addImage('reactangle',this.shapeSelectorID);
         this.addImage('rounded_rect',this.shapeSelectorID);
@@ -37,32 +38,56 @@ var ImageHolderContianer = (function(){
 })();
 
 
-
+// Shape Selector Class
 var ShapeSelector = (function(){
 
-    //hold the shapes ds on each shape selector
-    
-
-    var ShapeSelector = function(shapeSelectorID){
-        // Cto'r
-        var shapeSelectorID = this.shapeSelectorID;
+    // Cto'r 
+    var ShapeSelector = function(shapeSelectorID,imageHolderDS){
+        this.shapeSelectorID = shapeSelectorID;
+        this.imageHolderDS = imageHolderDS;
     }
 
-    ShapeSelector.prototype.initFN = function(fullOrEmpty){
+    ShapeSelector.prototype.drawContainer = function(baseElem){
      
-        // var DrawBase
-
-        if(fullOrEmpty === true){
-
-
-        }else{
-
+        var removeOldestList = function(){
+            var oldestUI = document.getElementsByTagName("ul")[0];
+            if(oldestUI !==undefined ){
+                oldestUI.parentNode.removeChild(oldestUI);
+            }
         }
+        
+        removeOldestList();
+        var ul = document.createElement("ul");
+        for (var key in this.imageHolderDS) {
+            var elem = this.imageHolderDS[key].loadedImageElem;
+            
+            var li = document.createElement("li");
+
+            (function(key,imageHolderDS){
+                li.addEventListener("click",function(){
+                    for (var keyT in imageHolderDS){
+                        imageHolderDS[keyT].selected = false;
+                    }
+                    imageHolderDS[key].selected = true;
+                    
+                });
+
+            })(key,this.imageHolderDS);
+
+            // Con't here - need to fix
+            if(elem.selected === true){
+                elem.class = "outline_selected";
+            }else{
+                elem.class = "";
+            }
+            li.appendChild(elem);
+            ul.appendChild(li);
+        }
+        baseElem.appendChild(ul);
     }
 
     return {
-        ShapeSelector:ShapeSelector,
-        // ShapesDS:ShapesDS
+        ShapeSelector:ShapeSelector
     };
 
 })();
