78df1b30b600c7373181c6b5d5f8337f67cb19a6
IOGHub <edanofek@gmail.com>
2018-02-10 01:17:35 +0200

    Add the ablity to divide by threes

--- 8.Convert number to words/src/Main.java
+++ 8.Convert number to words/src/Main.java
@@ -2,15 +2,36 @@
 import java.util.Arrays;
 
 
+import java.util.Hashtable;
+
+
 public class Main {
 
-    public static String convertNumberToWords(int number){
-        // con't here 
-        return "";
+    private Hashtable<Integer ,String> zeroToNine;
+    private Hashtable<Integer ,String> tenToNinety;
+    private Hashtable<Integer ,String> hundredThousend;
+
+    private static Integer  divider;// = 1000; 
+
+    public Main() {
+        super();
+        zeroToNine = new Hashtable<Integer ,String>();
+        tenToNinety = new Hashtable<Integer,String>();
+        divider = 1000;
+    }
+
+    private static String convertNumberToWords(Integer  number){
+        
+        if(number<divider){
+            return number.toString();
+        }
+        number/=(divider/10);
+        return convertNumberToWords(number%divider);
+        
     }
 
 	public static void main(String[] args) {
-        int number = 112;
+        Integer  number = 112;
         
         System.out.println("number intger");
         System.out.println(number);
