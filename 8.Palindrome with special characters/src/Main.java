d5d00208063b1a0679e52b8b5f0480e61073f8df
IOGHub <edanofek@gmail.com>
2018-03-10 00:34:12 +0200

    Update Question 8 file code

--- 8.Palindrome with special characters/src/Main.java
+++ 8.Palindrome with special characters/src/Main.java
@@ -1,29 +1,32 @@
-
-// import java.util.Arrays;
-
+import java.lang.String;
 
 public class Main {
-
     
+    public static String removeNotLatters(String strToRemoveRegex){
+        String pattern = "\\w";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(strToRemoveRegex);
+        return m.group(0);
+    }
     public static boolean isPoli(String checkPoli){
     
         int leftIndex =0,rightIndex = array.length-1;
-        final int numberToLeft = 0,numberToRight = 1;
-
-        do{
-
-            
-        }while(leftIndex<rightIndex);
+        
+        if(checkPoli.charAt(0) == checkPoli.charAt(checkPoli.length-1)){
+            return isPoli(checkPoli.substring(1,checkPoli.length - 2));
+        }
+        return false;
+        
     }
 
 	public static void main(String[] args) {
         String checkPoliandrome1 = "$XY123m422X*";
-        String checkPoliandromey2 = "$X123m4*22X*";
+        String checkPoliandrome2 = "$X123m4*22X*";
 
         System.out.println("mock array befroe switch");
-        System.out.println();
+        System.out.println(removeNotLatters(checkPoliandrome1));
         System.out.println("mock array after switch");
-        System.out.println();
+        System.out.println(removeNotLatters(checkPoliandrome2));
 	}
 
 }
