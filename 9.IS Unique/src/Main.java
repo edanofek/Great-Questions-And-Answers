82066226a8e8d0db7fcddeeeb84ab4514ec53af8
IOGHub <edanofek@gmail.com>
2018-03-25 21:08:39 +0300

    Update code file question 9

--- 9.IS Unique/src/Main.java
+++ 9.IS Unique/src/Main.java
@@ -4,9 +4,15 @@ import java.util.regex.Matcher;
 
 public class Main {
     
-  
+	public boolean isUnique(String str){
+
+	}
+
 	public static void main(String[] args) { 
-      
+	  String checkIsUnique1 = "#44",
+	  checkIsUnique2 = "#117",
+	  checkIsUnique3 = "#132";
+
 	}
 
 }
