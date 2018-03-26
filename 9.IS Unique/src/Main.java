bb39faac5dba0090efe3aaa00928f881a258a98a
IOGHub <edanofek@gmail.com>
2018-03-26 20:39:33 +0300

    Question 9 first soluation

--- 9.IS Unique/src/Main.java
+++ 9.IS Unique/src/Main.java
@@ -1,11 +1,32 @@
 import java.lang.String;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.lang.Integer;
+
+import java.util.Hashtable;
+
 
 public class Main {
     
-	public boolean isUnique(String str){
+	public static boolean isUnique(String str){
 
+		boolean isUniqueFlag = true;
+		int i=0;
+		Hashtable<Character,Integer> lattersTable =
+			 new Hashtable<Character,Integer>();
+
+		do{
+			 
+			Character key = str.charAt(i);
+
+			if(lattersTable.get(key) != null){
+				isUniqueFlag = false;
+			}
+
+			lattersTable.put(key,lattersTable.get(key) == null ? 1 : lattersTable.get(key)+1);
+			i++;
+
+		}while(isUniqueFlag == true && i< str.length() );
+
+		return isUniqueFlag;
 	}
 
 	public static void main(String[] args) { 
@@ -13,6 +34,9 @@ public class Main {
 	  checkIsUnique2 = "#117",
 	  checkIsUnique3 = "#132";
 
+		System.out.println(isUnique(checkIsUnique1));
+		System.out.println(isUnique(checkIsUnique2));
+		System.out.println(isUnique(checkIsUnique3));
 	}
 
 }
