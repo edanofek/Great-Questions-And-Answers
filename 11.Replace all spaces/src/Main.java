e8d07fe2cd428feeb20a649d596d9ded8fc4b169
IOGHub <edanofek@gmail.com>
2018-04-16 00:11:19 +0300

    Update readme for question 11

---
+++ 11.Replace all spaces/src/Main.java
@@ -0,0 +1,21 @@
+import java.lang.String;
+import java.lang.Integer;
+
+import java.util.Hashtable;
+
+public class Main {
+    
+
+	public static void main(String[] args) { 
+
+    String str1 = "7",
+    str2 = "84",
+    str3 = "722",
+    str4 = "737";
+
+    System.out.println(isPermutation(str1,str2));
+    System.out.println(isPermutation(str3,str4));
+                        
+	}
+
+}
