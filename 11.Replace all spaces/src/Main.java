b829f2ae2b40af467697d548013af6cadbefe1e7
IOGHub <edanofek@gmail.com>
2018-04-22 00:11:17 +0300

    Update question 11 code

--- 11.Replace all spaces/src/Main.java
+++ 11.Replace all spaces/src/Main.java
@@ -1,33 +1,56 @@
 import java.lang.String;
-import java.lang.Integer
+import java.lang.Integer;
 
-// import sun.security.util.Length;;
-
-import java.util.Hashtable;
 
 public class Main {
     
-    public static char[] repalceAllSpaces(char[] val,int length){
-
-        int newLength = Length;
-        char []retValue = new []char;
-        // TODO:Con't here
+    private static int returnNewLength(char[] val){
+        
+        int newLength = 0;
 
         for (char letter : val) {
             if(letter == ' '){
-            }else{
-
+                newLength += 2; //replace space with new length '%20' + 2 new chars places
+            }
+            else{
+                newLength += 1;
             }
         }
-        return '';
+
+        return newLength;
+
+    }
+
+    public static char[] repalceAllSpaces(char[] val,int length){
+        
+        int newLength = returnNewLength(val);
+        char[] retValue = new char[newLength];
+       
+        int index = 0;
+        for (char letter : val) {
+            if(letter == ' '){
+                retValue[index] = '%';
+                retValue[index+1] = '2';
+                retValue[index+2] = '0';
+                index += 2;
+            }
+            else{
+                retValue[index] = letter;
+                index += 1;
+            }
+        }
+
+        return retValue;
     }
 
 	public static void main(String[] args) { 
 
         int length = 13;
-        char []str = new char[length];
+        String str = "Mr John Smith "; 
+        char []val = str.toCharArray();
+        // TODO:Fix the value here
 
-        System.out.println(repalceAllSpaces(str,length));
+        System.out.println(repalceAllSpaces(val,length));
                         
 	}
 
