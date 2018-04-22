266733e247a9c580a863d451a90bb2c1b52c08e1
IOGHub <edanofek@gmail.com>
2018-04-23 00:50:57 +0300

    Upadte code question 11

--- 11.Replace all spaces/src/Main.java
+++ 11.Replace all spaces/src/Main.java
@@ -7,17 +7,41 @@ public class Main {
     private static int returnNewLength(char[] val){
         
         int newLength = 0;
+        int lastLatterPlace = getLastLatterPlace(val);
+        int valIndex = 0;
 
         for (char letter : val) {
             if(letter == ' '){
-                newLength += 2; //replace space with new length '%20' + 2 new chars places
-            }
-            else{
+                newLength += 3; //replace space with new length '%20' + 2 new chars places
+            }else{
                 newLength += 1;
             }
+
+            if(valIndex == lastLatterPlace){
+                break;
+            }
+            valIndex += 1;
         }
 
         return newLength;
+
+    }
+
+    private static int getLastLatterPlace(char []val) {
+        
+        int lastLatterPlace = 0;
+        int index =0;
+        for (char letter : val) {
+
+            if(letter != ' '){
+                lastLatterPlace = index;
+            }
+            
+            index +=1;
+            
+        }
+
+        return lastLatterPlace;
 
     }
 
@@ -25,19 +49,28 @@ public class Main {
         
         int newLength = returnNewLength(val);
         char[] retValue = new char[newLength];
-       
-        int index = 0;
+               
+        int index = 0,valIndex = 0;
+        int lastLatterPlace = getLastLatterPlace(val);
+
         for (char letter : val) {
-            if(letter == ' '){
+            if(letter == ' ' && index > 0  && valIndex < lastLatterPlace ){
                 retValue[index] = '%';
                 retValue[index+1] = '2';
                 retValue[index+2] = '0';
-                index += 2;
-            }
-            else{
+                index += 3;
+            }else {
                 retValue[index] = letter;
                 index += 1;
             }
+
+            
+            if(valIndex == lastLatterPlace){
+                
+                break;
+            }
+
+            valIndex += 1;
         }
 
         return retValue;
@@ -46,10 +79,8 @@ public class Main {
 	public static void main(String[] args) { 
 
         int length = 13;
-        String str = "Mr John Smith "; 
-        char []val = str.toCharArray();
-        // TODO:Fix the value here
-
+        char[] val = {'M','r',' ','J','o','h','n',' ','S','m','i','t','h',' '};
+        
         System.out.println(repalceAllSpaces(val,length));
                         
 	}
