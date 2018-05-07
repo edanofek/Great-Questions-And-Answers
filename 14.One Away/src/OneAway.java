af082910a484dc1e8c3916b0ad2f39d3deef4b36
Idan.Ofek <edanofek@gmail.com>
2018-05-08 00:27:55 +0300

    Update question 14 code

--- 14.One Away/src/OneAway.java
+++ 14.One Away/src/OneAway.java
@@ -8,10 +8,29 @@ public class OneAway {
     // 1.check for each word the numer of latters.
     // 2.check for each word the latter places.
     // 3.check for number of removeals (can be such as #1 with minus operation)
-    private Hashtable<Character,Integer> angramCheck;
+    // 4.need to rethink about how to solve this problem.
 
+    // private Hashtable<Character,Integer> angramCheck;
+
+    // Cto'r
     public OneAway(){
-        diffrentOfLaters = new Hashtable<Character,Integer>();
+        // diffrentOfLaters = new Hashtable<Character,Integer>();
+        
+    }
+
+    private boolean oneEdit(String word1,String word2) {
+
+        boolean retRes = true;
+        //word1 contains word2
+        if(word1.contains(word2)){
+            return true;
+        }else{
+            //only one edit action need to be made in order to support the  contains action
+            // TODO:Con't here
+        }
+        
+        //2.numbers of latter (don't must be relevent)
+        return retRes;
     }
 
     public static void main(String[] args){
