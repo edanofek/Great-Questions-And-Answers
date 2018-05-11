8882a1622185cd9819bac608e445ce0901a05f6b
Idan.Ofek <edanofek@gmail.com>
2018-05-12 00:07:51 +0300

    Question 14 source code

--- 14.One Away/src/OneAway.java
+++ 14.One Away/src/OneAway.java
@@ -16,43 +16,60 @@ public class OneAway {
 
     private boolean oneEdit(String word1,String word2) {
 
-        //if word1 contains word2 or word2 contains word1 
-        if(word1.contains(word2) || word2.contains(word1)){
+        //if word1 content equals word2  
+        if(word1.contentEquals(word2) ){
             return true;
         }else{
+            //comprasion length for both words can one or zero other wise false
+            if(Math.abs(word1.length() - word2.length()) > 1){
+                return false;
+            }else{ 
+                
+                String largerWord = word1.length() >= word2.length() ? word1 : word2;
+                String smallerWord = word1.length() < word2.length() ? word1 : word2;
+                
+                //let's check for only the same number of latters
+                int numbersOfEdits =0;
+                // pale, bale -> true --> change one lattar in word one or word two 
+                if(largerWord.length() == smallerWord.length()){
+                    for (int i=0;i<largerWord.length();i++) {
+                        if(largerWord.charAt(i) != smallerWord.charAt(i)){
+                            numbersOfEdits++;
+                        }    
+                        if(numbersOfEdits > 1){
+                            return false; //more than one edit
+                        }
+                    }
+                }else{
+                    
+                    // pale, ple -> true  --> add one latter in word one  or remove one latter in word two
+                    // abcde, abde -> true -- > need to add latter 'c' to word 2 or remove one latter
 
-            // What is the largest sub string
-            //only one edit action need to be made in order to support the  contains action
-            
-            // Ways to solve this problem:
-            // 1.if change one latter does the string will be contains -> O[N]
-            // 2.if remove one latter does the string will be contains -> O[N]
-            // 3.if add one latter does the string will be contains -> O[N]
+                    numbersOfEdits = 1; //base edit number (need to add one latter to smaller word)
+                    String addLatterWord;
+                    for(int i=0;i<largerWord.length();i++){
+                        if(!smallerWord.contains(new String(largerWord.charAt(i)))){
+                            //find the missing char @ place i
+                            addLatterWord = new String(
+                                smallerWord.substring(0, i)
+                                +new String(largerWord.charAt(i))
+                                +smallerWord.substring(i+1, smallerWord.length()-1));
+                            break; // exit the loop
+                        }
+                    }
 
-
-            // TODO:Con't here:
-            // pale, ple -> true  --> add one latter in word one  or remove one latter in word two
-            // pale, bale -> true --> change one lattar in word one or word two 
-            String largerWord = word1.length() >= word2.length() ? word1 : word2;
-            String smallerWord = word1.length() < word2.length() ? word1 : word2;
-
-            int iw1=0,iw2=0;
-            int numberOfMisses =0;
-
-            for(;iw1<smallerWord.length() && iw2 <largerWord.length();){
-                if(smallerWord.charAt(iw1) != largerWord.charAt(iw2)){
-                    numberOfMisses++;       
-                    iw2++;
+                    for (int i=0;i<largerWord.length();i++) {
+                        if(largerWord.charAt(i) != addLatterWord.charAt(i)){
+                            return false; //more than one edit
+                        }    
+                    }
                 }
-                if(numberOfMisses > 1)
-                    return false;
-                iw1++;
             }
             
+
         }
         
-        //2.numbers of latter (don't must be relevent)
-        return true;
+        
     }
 
    
