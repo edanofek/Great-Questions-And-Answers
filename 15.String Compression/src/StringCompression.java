6e90c9928ba9128c5c64197faa078a149f642599
Idan.Ofek <edanofek@gmail.com>
2018-05-14 00:14:25 +0300

    Update question 15 code

--- 15.String Compression/src/StringCompression.java
+++ 15.String Compression/src/StringCompression.java
@@ -15,13 +15,18 @@ public class StringCompression {
         int baseCount = str.length();
         String reString = new String();
 
-        // TODO:Con't here
+        int currentCharCounter = 1;
+        
         for(int i=0;i<str.length()-1;i++){
             if(str.charAt(i) == str.charAt(i+1)){
-                
+                currentCharCounter++;
+            }else{
+                reString+=String.valueOf(str.charAt(i))+String.valueOf(currentCharCounter);
+                currentCharCounter = 1;
             }
         }
-        return "";
+        reString+=String.valueOf(str.charAt(i))+String.valueOf(currentCharCounter);
+        return reString.length() >= str.length() ? str : reString;
     }
 
 }
